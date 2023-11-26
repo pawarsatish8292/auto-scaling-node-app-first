2a2ecc69a8e3c375d57065debcc59fc20b79f701
satish pawar <76686271+pawarsatish8292@users.noreply.github.com>
2023-11-26 22:47:40 +0530

    changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5001;
+const port = 3000;
 
 app.get('/',(res,req) => {
     res.send(`<h1>Autoscalling demo app </h1> <h4>Message : success </h4> <p> Version: 1.0.0`)
